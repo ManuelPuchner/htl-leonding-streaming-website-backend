a1e1b11a201ec87b9782ae662752721012c75083
LukMayr <l.mayrhuber@students.htl-leonding.ac.at>
2022-11-23 14:36:31 +0100

    feat: added member-router.get in file member-router-ts

--- src/repos/member-repo.ts
+++ src/repos/member-repo.ts
@@ -1,18 +1,52 @@
-import { Member} from "@prisma/client";
+import { Member, Tag } from "@prisma/client";
 import prisma from "../prisma";
 
 export async function getAllMembers(): Promise<Member[]> {
-    let members = await prisma.member.findMany();
+  let members = await prisma.member.findMany();
 
-    return members;
+  return members;
 }
 
 export async function getMemberById(id: number): Promise<Member> {
-    let member = await prisma.member.findUnique({
-        where:{
-            id: id
-        }
-    });
+  let member = await prisma.member.findUnique({
+    where: {
+      id: id,
+    },
+  });
 
-    return member;
-};
+  return member;
+}
+
+export async function createMember(name: string, tags: Tag[]): Promise<Member> {
+  //TODO: validation for member
+  let member = await prisma.member.create({
+    data: {
+      name: name,
+      tags: {
+        connect: tags,
+      },
+    },
+  });
+
+  return member;
+}
+
+export async function editMember(
+  id: number,
+  name: string,
+  tags: Tag[]
+): Promise<Member> {
+  //TODO: validation for member
+  let member = await prisma.member.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name: name,
+      tags: {
+        connect: tags,
+      },
+    },
+  });
+  return member;
+}
--- src/routers/member-router.ts
+++ src/routers/member-router.ts
@@ -3,18 +3,19 @@
 import { Member } from "@prisma/client";
 import express from "express";
 import { StatusCodes } from "http-status-codes";
+import { getAllMembers, getMemberById, createMember, editMember} from "../repos/member-repo";
 
 export const taskRouter = express.Router();
 
 // return all tasks
-taskRouter.get("/", function (request, response) {
-    const mebers: Member[] getAllMembers();
+taskRouter.get("/", async function (request, response) {
+    const mebers: Member[] = await getAllMembers();
     response.status(StatusCodes.OK).json(mebers);
 });
 
-taskRouter.get("/:id", function (request, response) {
+taskRouter.get("/:id", async function (request, response) {
     const id: number = Number(request.params.id);
-    const member : Member| undefined = getTaskById(id);
+    const member : Member | undefined = await getMemberById(id);
     if (member === undefined) {
         response.sendStatus(StatusCodes.NOT_FOUND);
         return;
