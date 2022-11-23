01ea3f4623a3113601eeccccc95c4ab2c3e2e812
LukMayr <l.mayrhuber@students.htl-leonding.ac.at>
2022-11-23 09:33:43 +0100

    fix: deleted unnecessary code

--- admin.ts
+++
@@ -1,5 +0,0 @@
-interface AdminComponent extends Member {
-    member: Member;
-    isAdmin: boolean;
-    password: string;
-}
--- member.ts
+++
@@ -1,5 +0,0 @@
-interface Member {
-  name: string;
-  id: number;
-  tag: string[]; // streaming, audio, video, etc
-}
