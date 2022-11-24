fa88b3112eaa5d0a403f76a6d11b2fa0d270972d
LukMayr <l.mayrhuber@students.htl-leonding.ac.at>
2022-11-24 21:52:07 +0100

    bug: fixed names

--- src/routers/member-router.ts
+++ src/routers/member-router.ts
@@ -7,15 +7,15 @@ import {
   editMember,
   deleteMember,
 } from "../repos/member-repo";
-export const taskRouter = express.Router();
+export const memberRouter = express.Router();
 
 // return all tasks
-taskRouter.get("/", async function (request, response) {
+memberRouter.get("/", async function (request, response) {
   const mebers: Member[] = await getAllMembers();
   response.status(200).json(mebers);
 });
 
-taskRouter.get("/:id", async function (request, response) {
+memberRouter.get("/:id", async function (request, response) {
   const id: number = Number(request.params.id);
   const member: Member | undefined = await getMemberById(id);
   if (member === undefined) {
@@ -25,13 +25,13 @@ taskRouter.get("/:id", async function (request, response) {
   response.status(200).json(member);
 });
 
-taskRouter.post("/", async function (request, response) {
+memberRouter.post("/", async function (request, response) {
   const { name, description, tagsIds, image } = request.body;
   const newMember = await createMember(name, description, tagsIds, image);
   response.status(200).json(newMember);
 });
 
-taskRouter.put("/:id", async function (request, response) {
+memberRouter.put("/:id", async function (request, response) {
   
   const id: number = Number(request.params);
   if (id === undefined) {
@@ -59,7 +59,7 @@ taskRouter.put("/:id", async function (request, response) {
   response.status(200).json(member);
 });
 
-taskRouter.delete("/:id", async function (request, response) {
+memberRouter.delete("/:id", async function (request, response) {
   const id: number = Number(request.params);
   if (id === undefined) {
     response.sendStatus(400);
