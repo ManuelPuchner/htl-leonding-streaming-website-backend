39c466acd13e333735168aaa078733980982f159
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-12-04 19:24:51 +0100

    tset

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,15 @@ app.use("/api/test", testRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}, http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}, http://localhost:${PORT}`);
+  console.log({
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET_KEY: process.env.AUTH_SECRET_KEY,
+  });
+  
+});
